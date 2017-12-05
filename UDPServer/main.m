3709c06e2a8af981daf7b316f3f0158bc8c4f49d
Matt Lintlop <matt_lintlop@icloud.com>
2017-12-05 11:25:59 -0800

    Initial Commit

---
+++ UDPServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UDPServer
+//
+//  Created by Matthew Lintlop on 12/5/17.
+//  Copyright © 2017 Matthew Lintlop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
